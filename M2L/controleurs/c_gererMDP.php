25eb78046d56e6a475b4215cbfec20f294845c6c
SunnyMustang <45700362+SunnyMustang@users.noreply.github.com>
2018-12-07 23:58:48 +0100

    Update c_gererMDP.php

--- M2L/controleurs/c_gererMDP.php
+++ M2L/controleurs/c_gererMDP.php
@@ -4,18 +4,21 @@ switch($action)
 {
 	case 'envoimdp':
 	{
-		//$stringmail="Bonjour, voici le lien pour saisir un nouveau mot de passe ";
-		//Envoie d'un email
-		//mail($mail, "Confirmation d'inscription", $stringmail, "From: M2L@gmail.com");
+		
 		include("./vues/v_mdpoublie.php");
 		break;
 	}
 	case 'mail':
-	{
+	{	
 		$adressemail=$_REQUEST['adressemail'];
+		
 		$mail=$pdo->retrouverMail($adressemail);
 		if($mail!=null)
 		{
+			//$stringmail="Bonjour, voici votre mot de passe : ".$mail[0]." Merci de votre confiance;
+			//Envoie d'un email
+			//mail($mail, "Oublie du mot de passe", $stringmail, "From: M2L@gmail.com");
+		
 			include("./vues/v_mailretrouve.php");}
 		else{
 			include("./vues/v_mauvaismail.php");
