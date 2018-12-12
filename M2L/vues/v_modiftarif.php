92e69f365c210c735106dc6ee116875832236319
deviljoew <zynx.nolimit@gmail.com>
2018-12-12 22:55:21 +0100

    Page tarif

--- M2L/vues/v_modiftarif.php
+++ M2L/vues/v_modiftarif.php
@@ -13,8 +13,19 @@
 </script>
 <div class="container-fluid">
   <div class="row">
-    <div class="col-md-12 setHeight" style="background-color:#00000055; opacity:0.9;text-align:center;height:740px;">
-    
+    <div class="col-md-4 setHeight" style="background-color:#00000055; opacity:0.9;height:740px;">
+      <p class="display-4" style="color:white;margin-left:20px;margin-top:10px;">Tarif kilométrique</p>
+      <p class="h5" style="color:white;margin-left:20px;margin-right:10px;">
+        <br>
+        Le barème kilométrique 2018 permet de calculer les frais et indemnités kilométriques, notamment dans le cadre de la déduction des frais réels, pour la déclaration de revenus 2018.<br><br>
+
+
+      Le barème kilométrique 2018 a été publié par l'administration fiscale le 24 janvier 2018. Il reste le même que celui applicable l'an dernier.<br><br>
+      Le barème qui suit est utilisable pour les voitures. Si le déplacement se fait en deux-roues dans le cadre du travail, vous devez utiliser le barème kilométrique moto et deux-roues.<br><br>
+      Le barème kilométrique 2019 n'est pas encore connu. Il devrait normalement être publié par les impôts en janvier-février 2019.  </p>
+    </div>
+    <div class="col-md-8 setHeight" style="background-color:#00000055; opacity:0.9;height:740px;">
+
       <?php
         if(isset($erreurs)){
           include("./vues/v_erreurs.php");
@@ -23,25 +34,18 @@
           include("./vues/v_message.php");
         }
       ?>
-      <p class="Display-4" style="font-size:18px;color:white;">
-        <strong>Tarif kilométrique actuel utilisé pour les frais de déplacement(s) des demandeurs</strong><br>
-        
-      </p>
-<form name="modiftarif" style="margin:50px;" action="index.php?uc=Profil&action=misAjoursTarif" onSubmit="return verifChamps();" method="POST">
-      <p class="display-4" style="font-size:15px;text-align:center;color:white;"><input type="text"  class="form-control form-control-sm" value="<?php echo $tarifkm; ?>" id="tarif" name="tarif" placeholder="Tarif km"><br>
-        
-      </p>
-     
-      <button onClick="return verifChamps();" style="width:100%;" class="btn btn-success">Valider le nouveau tarif km</button>
-           
-</form> 
-<p class="Display-4" style="font-size:18px;color:white;">
-        Le barème kilométrique 2018 permet de calculer les frais et indemnités kilométriques, notamment dans le cadre de la déduction des frais réels, pour la déclaration de revenus 2018. 
-     
-
-      Le barème kilométrique 2018 a été publié par l'administration fiscale le 24 janvier 2018. Il reste le même que celui applicable l'an dernier. 
-      Le barème qui suit est utilisable pour les voitures. Si le déplacement se fait en deux-roues dans le cadre du travail, vous devez utiliser le barème kilométrique moto et deux-roues.
-      Le barème kilométrique 2019 n'est pas encore connu. Il devrait normalement être publié par les impôts en janvier-février 2019.  </p>
-    </div>
+        <p class="display-4" style="color:white;margin-left:50px;margin-top:10px;">Tarif kilométrique actuel utilisé pour le(s) frais de déplacement(s) des demandeurs</p>
+      <form name="modiftarif" style="margin:50px;" action="index.php?uc=Profil&action=misAjoursTarif" onSubmit="return verifChamps();" method="POST">
+        <table class="table table-light table-bordered">
+          <tr>
+            <td style="text-align:center;">Tarif kilométrique</td>
+            <td style="text-align:center;"><input  TITLE="Modifier le profil" type="text" class="form-control form-control-sm" value="<?php echo $tarifkm; ?>" id="tarif"  name="tarif" placeholder="Tarif km"></td>
+          </tr>
+          <tr>
+            <td colspan="2" style="text-align:center;"><button onClick="return verifChamps();" style="width:400px;margin-right:150px;" class="btn btn-success">Valider le nouveau tarif kilométrique</button>
+            <a href="index.php?uc=Profil&action=voirTarif" TITLE="Modifier le profil" style="width:400px;" class="btn btn-dark">Annuler</a></td>
+          </tr>
+        </table>
+      </form>
   </div>
 </div>
--- M2L/vues/v_tarif.php
+++ M2L/vues/v_tarif.php
@@ -1,7 +1,18 @@
 <div class="container-fluid">
   <div class="row">
-    <div class="col-md-12 setHeight" style="background-color:#00000055; opacity:0.9;text-align:center;height:740px;">
-    
+    <div class="col-md-4 setHeight" style="background-color:#00000055; opacity:0.9;height:740px;">
+      <p class="display-4" style="color:white;margin-left:20px;margin-top:10px;">Tarif kilométrique</p>
+      <p class="h5" style="color:white;margin-left:20px;margin-right:10px;">
+        <br>
+        Le barème kilométrique 2018 permet de calculer les frais et indemnités kilométriques, notamment dans le cadre de la déduction des frais réels, pour la déclaration de revenus 2018.<br><br>
+
+
+      Le barème kilométrique 2018 a été publié par l'administration fiscale le 24 janvier 2018. Il reste le même que celui applicable l'an dernier.<br><br>
+      Le barème qui suit est utilisable pour les voitures. Si le déplacement se fait en deux-roues dans le cadre du travail, vous devez utiliser le barème kilométrique moto et deux-roues.<br><br>
+      Le barème kilométrique 2019 n'est pas encore connu. Il devrait normalement être publié par les impôts en janvier-février 2019.  </p>
+    </div>
+    <div class="col-md-8 setHeight" style="background-color:#00000055; opacity:0.9;height:740px;">
+
       <?php
         if(isset($erreurs)){
           include("./vues/v_erreurs.php");
@@ -10,24 +21,13 @@
           include("./vues/v_message.php");
         }
       ?>
-      <p class="Display-4" style="font-size:18px;color:white;">
-        <strong>Tarif kilométrique actuel utilisé pour les frais de déplacement(s) des demandeurs</strong><br>
-        
-      </p>
-      <p class="display-4" style="font-size:15px;text-align:center;color:white;"><img src="./images/tarif.jpg" width="30px" height="30px"/>
-        <?php echo $tarifkm.' €';?><br>
-        
-      </p>
-      
-        <a TITLE="Modifier le tarif kilométrique" href="index.php?uc=Profil&action=modifiertarif"><button style="width:100%;" class="btn btn-success">Modifier tarif km</button></a>
-           
-           <p class="Display-4" style="font-size:18px;color:white;">
-        Le barème kilométrique 2018 permet de calculer les frais et indemnités kilométriques, notamment dans le cadre de la déduction des frais réels, pour la déclaration de revenus 2018. 
-     
+      <p class="display-4" style="color:white;margin-left:50px;margin-top:10px;">Tarif kilométrique actuel utilisé pour le(s) frais de déplacement(s) des demandeurs</p><br>
+      <p class="display-4" style="font-size:25px;text-align:center;color:white;"><img src="./images/tarif.png" width="60px" height="60px"/>
+        <strong><?php echo $tarifkm.' €';?></strong><br>
 
-      Le barème kilométrique 2018 a été publié par l'administration fiscale le 24 janvier 2018. Il reste le même que celui applicable l'an dernier. 
-      Le barème qui suit est utilisable pour les voitures. Si le déplacement se fait en deux-roues dans le cadre du travail, vous devez utiliser le barème kilométrique moto et deux-roues.
-      Le barème kilométrique 2019 n'est pas encore connu. Il devrait normalement être publié par les impôts en janvier-février 2019.  </p>
+      </p>
+
+        <a TITLE="Modifier le tarif kilométrique" href="index.php?uc=Profil&action=modifiertarif"><button style="width:100%;" class="btn btn-success">Modifier le tarif kilométrique</button></a>
 
     </div>
   </div>
