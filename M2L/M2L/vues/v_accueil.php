a837ee9dc0ef5c8b9a53d80db82a8a0bfbc10c88
deviljoew <zynx.nolimit@gmail.com>
2018-12-14 18:58:15 +0100

    1

--- M2L/M2L/vues/v_accueil.php
+++
@@ -1,3 +0,0 @@
-<div id="accueil">
-Veuillez vous connecter pour continuer
-</div>
--- M2L/M2L/vues/v_message.php
+++
@@ -1,5 +0,0 @@
-<div class="container-fluid" style="border-radius: 5px;background-color:#4dffb899;">
-	<div style="text-align:center;color:white;">
-		<span class="Display-4" style="font-size:23px;"><?php echo $message;?></span>
-	</div>
-</div>
