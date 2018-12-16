22b082094296ec9ef7e52d3f5de2bb38e122bd6f
deviljoew <zynx.nolimit@gmail.com>
2018-12-17 00:59:37 +0100

    Update v_pdfCERFA.php

--- M2L/vues/v_pdfCERFA.php
+++ M2L/vues/v_pdfCERFA.php
@@ -9,14 +9,14 @@ function Header()
     // Logo
     $this->Image('./images/cerfa.png',10,6,30);
     // Police Arial gras 15
-    $this->SetFont('Arial','B',14);
+    $this->SetFont('$fontName','B',14);
     // Décalage à droite
     $this->Cell(80);
     // Titre
     $this->Cell(30,10,'Reçu au titre des dons à \n certains organismes d\'intérêt général',0,0,'C');
     // Saut de ligne
     $this->Ln(20);
-    $this->SetFont('Arial','',12);
+    $this->SetFont('$fontName','',12);
     //n° CERFA
     $this->Cell(30,10,'N° 115080*02',0,0,'C');
     // Décalage à droite
@@ -24,7 +24,7 @@ function Header()
     // Numero du recu
     $this->Cell(30,10,'Numero d\'orde du reçu:',0,1,'C');
     $this->Cell(150);
-    $this->SetFont('Arial','B',12);
+    $this->SetFont('$fontName','B',12);
     $this->Cell(30,10,'2016-14',1,1,'C');
 
 
@@ -37,7 +37,7 @@ function Footer()
     // Positionnement à 1,5 cm du bas
     $this->SetY(-15);
     // Police Arial italique 8
-    $this->SetFont('Arial','I',8);
+    $this->SetFont('$fontName','I',8);
     // Numéro de page
     $this->Cell(0,10,'Page '.$this->PageNo().'/1',0,0,'C');
 }
@@ -55,20 +55,23 @@ function LoadData($file)
 }
 
 $pdf = new PDF();
+// Add Unicode fonts (.ttf files)
+$fontName = 'Calibri';
+$pdf->AddFont($fontName,'','C:\Windows\Fonts\Calibri.ttf',true);
 $pdf->AddPage();
 $pdf->SetTitle('Reçu CERFA don',true);
-$pdf->SetFont('Arial','B',14);
+$pdf->SetFont('$fontName','B',14);
 $pdf->SetFillColor(210,210,200);
 $pdf->Cell(190, 8, "Bénéficiaire de versements", 1, 1, "C", true);
-$pdf->SetFont('Arial','',12);
+$pdf->SetFont('$fontName','',12);
 $pdf->MultiCell(190,5, "\nNom ou dénomination : \n
 Adresse : \n
 ............................................................................................................................................................... \n
                                        Oeuvre ou organisme d'intérêt général \n ", 1, "L", false);
-$pdf->SetFont('Arial','B',14);
+$pdf->SetFont('$fontName','B',14);
 $pdf->SetFillColor(210,210,200);
 $pdf->Cell(190, 8, "Donateur", 1, 1, "C", true);
-$pdf->SetFont('Arial','',12);
+$pdf->SetFont('$fontName','',12);
 $pdf->MultiCell(190,8, "\nNom : \nAdresse : \nCode postal :                     Commune : \n ", 1, "L", false);
 $pdf->MultiCell(190,8, "\nLe bénéficiaire reconnait avoir reçu au titre des versements ouvrant droit à réduction d'impôt, la somme de : \n
 Somme en toute lettre : \n
@@ -81,7 +84,7 @@ Motif du versement : Autres \n
 $header = array('Pays', 'Capitale', 'Superficie (km²)', 'Pop. (milliers)');
 // Chargement des données
 $data = 'pays.txt';
-$pdf->SetFont('Arial','',14);
+$pdf->SetFont('$fontName','',14);
 $pdf->Output();
 
 ?>
