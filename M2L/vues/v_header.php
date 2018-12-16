9bca26a74a67e3893d40b41cfb905131122d0b25
deviljoew <zynx.nolimit@gmail.com>
2018-12-16 15:12:13 +0100

    1

--- M2L/vues/v_header.php
+++ M2L/vues/v_header.php
@@ -20,7 +20,7 @@ if(!isset($_REQUEST['uc'])){
         </div>
 			</div>
 			<nav class="navbar navbar-expand-lg navbar-dark hoverNav" style="background-color: black;">
-				<span class="navbar-brand hoverNav" disabled="disabled">Menu</span>
+				<span class="navbar-brand hoverNav" disabled="disabled"><img src="./images/fleche.png" width='40px'></span>
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
 				</button>
